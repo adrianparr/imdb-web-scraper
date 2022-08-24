3fd9b5055b4e0792934171ba478428c6376880c3
Adrian Parr <adrianparr@gmail.com>
2022-08-24 10:56:20 +0100

    Added actors, director, writers

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const Fs = require('fs')
 const Path = require('path')
 
 const IMDB_IDs = ['https://www.imdb.com/title/tt0110912/', 'tt0109830', 'https://www.imdb.com/title/tt10738906/']
+// const IMDB_IDs = ['tt0110912']
 const PORT = 8000
 const arrayOfData = []
 
@@ -47,6 +48,34 @@ const extractData = (id, html) => {
   data.plot = $(
     '#__next > main > div > section.ipc-page-background.ipc-page-background--base.sc-ca85a21c-0.efoFqn > section > div:nth-child(4) > section > section > div.sc-2a827f80-2.kqTacj > div.sc-2a827f80-10.fVYbpg > div.sc-2a827f80-4.bWdgcV > div.sc-16ede01-9.bbiYSi.sc-2a827f80-11.kSXeJ > div.sc-16ede01-7.hrgVKw > span.sc-16ede01-1.kgphFu'
   ).text()
+
+  data.director = $(
+    '#__next > main > div > section.ipc-page-background.ipc-page-background--base.sc-ca85a21c-0.efoFqn > section > div:nth-child(4) > section > section > div.sc-2a827f80-2.kqTacj > div.sc-2a827f80-10.fVYbpg > div.sc-2a827f80-4.bWdgcV > div.sc-fa02f843-2.dwQKsL > div > div > ul > li:nth-child(1) > div > ul > li > a'
+  ).text()
+
+  const writersArray = []
+  const writersWrapperEl = $(
+    '#__next > main > div > section.ipc-page-background.ipc-page-background--base.sc-ca85a21c-0.efoFqn > section > div:nth-child(4) > section > section > div.sc-2a827f80-2.kqTacj > div.sc-2a827f80-10.fVYbpg > div.sc-2a827f80-4.bWdgcV > div.sc-fa02f843-0.fjLeDR > ul > li:nth-child(2) > div > ul > li.ipc-inline-list__item'
+  )
+  writersWrapperEl.each((index, element) => {
+    const name = $('a.ipc-metadata-list-item__list-content-item', element).text()
+    writersArray.push(name)
+  })
+  data.writers = writersArray.join(', ')
+
+  // data.writer = $(
+  //   '#__next > main > div > section.ipc-page-background.ipc-page-background--base.sc-ca85a21c-0.efoFqn > section > div:nth-child(4) > section > section > div.sc-2a827f80-2.kqTacj > div.sc-2a827f80-10.fVYbpg > div.sc-2a827f80-4.bWdgcV > div.sc-fa02f843-2.dwQKsL > div > div > ul > li:nth-child(2) > div > ul > li > a'
+  // ).text()
+
+  const topCastWrapperEl = $(
+    '#__next > main > div > section.ipc-page-background.ipc-page-background--base.sc-ca85a21c-0.efoFqn > div > section > div > div.sc-18496876-1.bxQyHL.ipc-page-grid__item.ipc-page-grid__item--span-2 > section.ipc-page-section.ipc-page-section--base.sc-36c36dd0-0.bfKRTS.title-cast.title-cast--movie.celwidget > div.ipc-shoveler.ipc-shoveler--base.ipc-shoveler--page0.title-cast__grid > div.ipc-sub-grid.ipc-sub-grid--page-span-2.ipc-sub-grid--wraps-at-above-l.ipc-shoveler__grid > div > div.sc-36c36dd0-8.fSYMLK'
+  )
+  const actorsArray = []
+  topCastWrapperEl.each((index, element) => {
+    const name = $('a[data-testid="title-cast-item__actor"]', element).text()
+    actorsArray.push(name)
+  })
+  data.actors = actorsArray.join(', ')
   const srcset = $(
     '#__next > main > div > section.ipc-page-background.ipc-page-background--base.sc-ca85a21c-0.efoFqn > section > div:nth-child(4) > section > section > div.sc-2a827f80-2.kqTacj > div.sc-2a827f80-3.dhWlsy > div > div.sc-77a2c808-2.mcnrT > div > div > div.ipc-media.ipc-media--poster-27x40.ipc-image-media-ratio--poster-27x40.ipc-media--baseAlt.ipc-media--poster-l.ipc-poster__poster-image.ipc-media__img > img'
   ).attr('srcset')
@@ -60,9 +89,9 @@ const extractData = (id, html) => {
     '#__next > main > div > section.ipc-page-background.ipc-page-background--base.sc-ca85a21c-0.efoFqn > section > div:nth-child(4) > section > section > div.sc-2a827f80-2.kqTacj > div.sc-2a827f80-10.fVYbpg > div.sc-2a827f80-4.bWdgcV > div.sc-16ede01-9.bbiYSi.sc-2a827f80-11.kSXeJ > div.ipc-chip-list--baseAlt.ipc-chip-list.sc-16ede01-5.ggbGKe > div.ipc-chip-list__scroller'
   ).text()
   data.genres = genres
-    .replace(/([A-Z])/g, ',$1')
+    .replace(/([A-Z])/g, ', $1')
     .trim()
-    .substring(1)
+    .substring(2)
   return data
 }
 
