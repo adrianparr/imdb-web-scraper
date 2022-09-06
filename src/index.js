bc64353768e3be3ea62979fe2e35d2240106cd98
Adrian Parr <adrianparr@gmail.com>
2022-09-06 17:27:21 +0100

    Added require(‘cors’)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const axios = require('axios').default
 const cheerio = require('cheerio')
+const cors = require('cors')
 const express = require('express')
+
 const PORT = process.env.PORT || 3001
 const app = express()
 
